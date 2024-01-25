35b15b0b12210b5ae3f9650e763fdd38c53db7c7
naingkhanthtet <hoceice420@gmail.com>
2024-01-25 21:55:02 +0630

    created package and added Java file

--- src/main/java/com/napier/team4/Main.java
+++ src/main/java/com/napier/team4/Main.java
@@ -2,6 +2,6 @@ package com.napier.team4;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello team 4!");
+        System.out.println("Hello team 4");
     }
 }
