51e724eef5f83291f25f339a6cdc94a4cd21d2d6
Htoisanaung <justincoffee46@gmail.com>
2024-02-01 01:27:19 +0630

    Added MYSQLConnection's connect method and disconnect method in main.java.

--- src/main/java/com/napier/team4/Main.java
+++ src/main/java/com/napier/team4/Main.java
@@ -1,9 +1,13 @@
 // This file is the starting point of the whole app.
 package com.napier.team4;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
         // Connect to MYSQL
         MYSQLConnection mysqlCon = new MYSQLConnection();
+        mysqlCon.connect();
+        mysqlCon.disconnect();
     }
 }
