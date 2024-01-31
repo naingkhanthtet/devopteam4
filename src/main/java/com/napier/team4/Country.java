4c7ec39c99bc5e3ab7d9c55c3956bb784efc56bd
Bhone Pyae Aung <bhonepyaeaung15@gmail.com>
2024-02-01 02:05:01 +0630

    Created Country.java file

---
+++ src/main/java/com/napier/team4/Country.java
@@ -0,0 +1,63 @@
+package com.napier.team4;
+
+public class Country {
+    private String code;
+    private String name;
+    private String continent;
+    private String region;
+    private String population;
+    private String capital;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
