cb8d8c548773ff8c94444a496c3355ac2c462825
Bhone Pyae Aung <bhonepyaeaung15@gmail.com>
2024-02-01 21:08:36 +0630

    Create local variable for TOP N

--- src/main/java/com/napier/team4/Main.java
+++ src/main/java/com/napier/team4/Main.java
@@ -5,11 +5,13 @@ import java.sql.*;
 import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
+        int topN = 5; // local variable for getting TOP N
+
         // Connect to MYSQL
         MYSQLConnection mysqlCon = new MYSQLConnection();
         Connection con = mysqlCon.connect();
+
         CountryReporter countryReporter = new CountryReporter();
         // For task 1
         List<Country> sortedCountries = countryReporter.sortCountryByPopulation(con);
@@ -18,17 +20,19 @@ public class Main {
         // For task 3
         List<Country> sortedCountriesByRegion = countryReporter.sortCountryByPopulationBasedOnRegion(con, "Caribbean");
         // For task 4
-        List<Country> topNPopulatedCountries= countryReporter.getTopNPopulatedCountries(con, 5);
+        List<Country> topNPopulatedCountries= countryReporter.getTopNPopulatedCountries(con, topN);
         // For task 5
-        List<Country> topNPopulatedCountriesInContinent = countryReporter.getTopNPopulatedCountriesInContinent(con, "Asia", 5);
+        List<Country> topNPopulatedCountriesInContinent = countryReporter.getTopNPopulatedCountriesInContinent(con, "Asia", topN);
         // For task 6
-        List<Country> topNPopulatedCountriesInRegion = countryReporter.getTopNPopulatedCountriesInRegion(con, "Caribbean", 5);
+        List<Country> topNPopulatedCountriesInRegion = countryReporter.getTopNPopulatedCountriesInRegion(con, "Caribbean", topN);
+
         countryReporter.displayCountryInfo(sortedCountries);
         countryReporter.displayCountryInfo(sortedCountriesByContinent);
         countryReporter.displayCountryInfo(sortedCountriesByRegion);
         countryReporter.displayCountryInfo(topNPopulatedCountries);
         countryReporter.displayCountryInfo(topNPopulatedCountriesInContinent);
         countryReporter.displayCountryInfo(topNPopulatedCountriesInRegion);
+
         mysqlCon.disconnect();
     }
 }
