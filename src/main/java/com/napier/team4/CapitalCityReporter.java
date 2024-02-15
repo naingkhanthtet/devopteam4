29e5aa0e8b130a729237bf73b261af2a87e66eed
Bhone Pyae Aung <bhonepyaeaung15@gmail.com>
2024-02-15 08:00:11 +0630

    Implement getTopNPopulatedCapitalCitiesInContinent and getTopNPopulatedCapitalCitiesInRegion

--- src/main/java/com/napier/team4/CapitalCityReporter.java
+++ src/main/java/com/napier/team4/CapitalCityReporter.java
@@ -170,7 +170,93 @@ public class CapitalCityReporter {
         }
     }
 
+    /**
+     * Retrieves the top N populated CapitalCity objects representing capital cities in a continent,
+     * organized by largest population to smallest.
+     *
+     * @param con       The database connection.
+     * @param continent The continent for which top populated capital cities are to be retrieved.
+     * @param topN         The number of top populated capital cities to retrieve.
+     * @return A list of CapitalCity objects sorted by population in descending order, limited to the top N.
+     */
+    public List<CapitalCity> getTopNPopulatedCapitalCitiesInContinent(@NotNull Connection con, String continent, int topN) {
+        List<CapitalCity> capitalCities = new ArrayList<>();
+        try {
+            // Use a PreparedStatement to prevent SQL injection
+            String strSelect = "SELECT city.Name as CityName, country.Name as CountryName, city.Population " +
+                    "FROM city " +
+                    "JOIN country ON city.CountryCode = country.Code " +
+                    "WHERE city.ID = country.Capital AND country.Continent = ? " +
+                    "ORDER BY Population DESC " +
+                    "LIMIT ?";
 
+            PreparedStatement pstmt = con.prepareStatement(strSelect);
+            // Set parameters for the continent and the limit (top N)
+            pstmt.setString(1, continent);
+            pstmt.setInt(2, topN);
+
+            // Execute SQL statement
+            ResultSet rset = pstmt.executeQuery();
+            // Iterate through the result set and create CapitalCity objects
+            while (rset.next()) {
+                CapitalCity capitalCity = createCapitalCityFromResultSet(rset);
+                capitalCities.add(capitalCity);
+            }
+
+            // Close the ResultSet and Statement
+            rset.close();
+            pstmt.close();
+
+            return capitalCities;
+        } catch (SQLException e) {
+            handleSQLException(e);
+            return null;
+        }
+    }
+
+    /**
+     * Retrieves the top N populated CapitalCity objects representing capital cities in a region,
+     * organized by largest population to smallest.
+     *
+     * @param con    The database connection.
+     * @param region The region for which top populated capital cities are to be retrieved.
+     * @param topN   The number of top populated capital cities to retrieve.
+     * @return A list of CapitalCity objects sorted by population in descending order, limited to the top N.
+     */
+    public List<CapitalCity> getTopNPopulatedCapitalCitiesInRegion(@NotNull Connection con, String region, int topN) {
+        List<CapitalCity> capitalCities = new ArrayList<>();
+        try {
+            // Use a PreparedStatement to prevent SQL injection
+            String strSelect = "SELECT city.Name as CityName, country.Name as CountryName, city.Population " +
+                    "FROM city " +
+                    "JOIN country ON city.CountryCode = country.Code " +
+                    "WHERE city.ID = country.Capital AND country.Region = ? " +
+                    "ORDER BY Population DESC " +
+                    "LIMIT ?";
+
+            PreparedStatement pstmt = con.prepareStatement(strSelect);
+            // Set parameters for the region and the limit (top N)
+            pstmt.setString(1, region);
+            pstmt.setInt(2, topN);
+
+            // Execute SQL statement
+            ResultSet rset = pstmt.executeQuery();
+            // Iterate through the result set and create CapitalCity objects
+            while (rset.next()) {
+                CapitalCity capitalCity = createCapitalCityFromResultSet(rset);
+                capitalCities.add(capitalCity);
+            }
+
+            // Close the ResultSet and Statement
+            rset.close();
+            pstmt.close();
+
+            return capitalCities;
+        } catch (SQLException e) {
+            handleSQLException(e);
+            return null;
+        }
+    }
 
 
     /**
